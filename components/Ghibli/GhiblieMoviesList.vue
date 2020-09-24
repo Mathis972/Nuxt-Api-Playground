f5c8dd18d0c9560b0c0d1b194427ab53ac7a97a2
EyZex <eyzex@hotmail.fr>
2020-09-24 16:03:26 +0200

    first commit

---
+++ components/Ghibli/GhiblieMoviesList.vue
@@ -0,0 +1,20 @@
+<template>
+  <p v-if="!movies">Fetching Movies</p>
+
+  <div v-else>
+    <h1>Movies</h1>
+    <ul>
+      <li v-for="movie of movies" :key="movie.id">{{ movie.title }}</li>
+    </ul>
+    <button @click="$fetch">Refresh</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["movies"],
+};
+</script>
+
+<style>
+</style>
---
+++ pages/ghibli.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img src="/ghibli.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;PD.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+      <GhiblieMoviesList :movies="moviesList" />
+    </v-flex>
+  </v-layout>
+</template>
+
+
+<script>
+import GhiblieMoviesList from "../components/Ghibli/GhiblieMoviesList";
+export default {
+  async asyncData({ $axios }) {
+    const moviesList = await $axios.$get(
+      "https://ghibliapi.herokuapp.com/films"
+    );
+    return { moviesList };
+  },
+};
+</script>
+
+<style>
+</style>
